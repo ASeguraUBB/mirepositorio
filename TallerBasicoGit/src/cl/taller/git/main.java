a8b78e4e0868655fffb72c60bde8b18ba459e2cc
janysegura <janysegura@users.noreply.github.com>
2014-04-04 10:56:07 -0400

    comita feliz
    agregue una coma y una carita

--- TallerBasicoGit/src/cl/taller/git/main.java
+++ TallerBasicoGit/src/cl/taller/git/main.java
@@ -4,7 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		System.out.println("Segundo comit");
-		System.out.println("Linea agregada por web");
+		System.out.println("agrego una coma , ^^ por web");
 
 	}
 }
